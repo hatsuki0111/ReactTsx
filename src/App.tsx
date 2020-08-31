f70efcd72a47c1f94e55b88288eee60db71a2bd0
hatsuki0111 <cist.b217.h.saitou@gmail.com>
2020-08-31 20:46:06 +0900

    オブジェクト配列の表示 子コンポーネントを学ぶ

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Child from './components/Child';
 
-function App() {
+const App: React.FC = () => {
+  type Item ={
+    id: number
+    title: string
+  }
+
+  const items: Item[] = [
+    {
+      id:1,
+      title:'寿司'
+    },
+    {
+      id: 2,
+      title: '一番ださい商品'
+    },
+  ]
+
+  const message: string='React'
   return (
-    
-    <div className="App"><p>Hello</p></div>
+    <div className="App">
+      <p>Hello</p>
+      <p>{message}</p>
+      <ul>
+          {items.map((item: Item) =>(
+          <li key={item.id}>{item.title}</li>
+          ))}
+      </ul>
+      <Child />
+    </div>
       );
 }
 
---
+++ src/components/Child.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Child: React.FC = () =>{
+  return(
+    <p>子コンポーネント</p>
+  )
+}
+
+export default Child
